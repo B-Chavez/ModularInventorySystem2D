ab490d8d5804ec5d731938ad4bd061070bff390e
B-Chavez <59714905+B-Chavez@users.noreply.github.com>
2025-06-03 23:05:53 -0700

    Added stackable system (bugged)
    I added a stackable system, I feel like I'm close to figuring it out

--- InventorySystem/Inventory.gd
+++ InventorySystem/Inventory.gd
@@ -2,10 +2,18 @@ extends Node
 class_name InventoryMaster
 
 var inventoryContainer: Array = [] #Stores each item in here
-@export var inventoryMaxSize: int = 1 #Inventory maximum size, note this is not a constant, so if you wanna change it go ahead. It might break the UI though if you go to high
+@export var inventoryMaxSize: int = 10 #Inventory maximum size, note this is not a constant, so if you wanna change it go ahead. It might break the UI though if you go to high
+
+var tempItem = preload("res://items/soda_can.tres")
+var tempItem2 = preload("res://items/soda_can2.tres")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	add_item(tempItem)
+	add_item(tempItem2)
+	for slot in inventoryContainer.size():
+		print(inventoryContainer[slot])
 	pass # Replace with function body.
 
 
@@ -16,8 +24,19 @@ func _process(delta: float) -> void:
 func add_item(item: Resource) -> bool: # -> bool means you're going to return a boolean
 	if is_full(): #If is_full returns true, which means the inventory is full, then return false to whatever called add_item
 		return false
-	#Otherwise add the item, which is a Resource, to the inventoryContainer and return true to whatever called add_item
-	inventoryContainer.append(item)
+	
+	#Check if the item exists and is stackable
+	for slot in inventoryContainer:
+
+		if slot["item"].id == item.id and slot["item"].stackable:
+			slot["item"].count += 1
+			return true
+	
+	#Otherwise, place in a new slot
+	inventoryContainer.append(
+		{"item": item, "count":1}
+	)
+	print("Line 39 ", inventoryContainer)
 	return true	
 	
 # Remove item that has been passed in when function was called from inventoryContainer array
