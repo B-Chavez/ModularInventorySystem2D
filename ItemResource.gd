a5cb3d0df6c3862e845102d95cfdf1b7d8230182
B-Chavez <59714905+B-Chavez@users.noreply.github.com>
2025-06-05 00:30:37 -0700

    Fixed ItemResource.gd
    Changed the id in ItemResource to an int

--- ItemResource.gd
+++ ItemResource.gd
@@ -1,7 +1,7 @@
 extends Resource
 class_name ItemResource
 
-@export var id: String
+@export var id: int
 @export var name: String
 @export var texture: Texture2D
 @export var stackable: bool = false
