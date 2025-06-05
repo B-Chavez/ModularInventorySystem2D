73fb9a304c8713d5d1e19f744f72db10f6a8084e
B-Chavez <59714905+B-Chavez@users.noreply.github.com>
2025-06-05 00:15:03 -0700

    Changed use of dictionaries to classes
    I removed the dictionary that's instantiated multiple times to be item slots and replaced them with a class that's instantiated for each slot. I also replaced all the references to the dictionary with references to class.

---
+++ InventorySystem/Core_scripts/InventorySlot.gd
@@ -0,0 +1,6 @@
+extends Resource
+
+class_name InventorySlot
+
+var item: ItemResource = null
+var count: int = 0
--- InventorySystem/Inventory.gd
+++ InventorySystem/Inventory.gd
@@ -20,18 +20,18 @@ func _ready() -> void:
 		add_item(tempItem)
 	pass # Replace with function body.
 
-	remove_item(tempItem, 3, 3) #passing in three arguements so we can remove the specific item, determine the location you're trying to remove the item from, and how many you want to remove
-	print("Amount of filled slots in inventory: ", inventoryContainer.size())
+	remove_item(3, 3) #passing in two arguements so we can determine the location you're trying to remove the item from, and how many you want to remove
+	print("Line 24 | Amount of filled slots in inventory: ", inventoryContainer.size())
 	for slot in inventoryContainer.size():
-		print("Slot ", slot, " ",inventoryContainer[slot]["item"], " Quantity in this slot: ", inventoryContainer[slot]["count"])
-	print(inventoryContainer[3])	
+		print("Line 26 | Slot ", slot, " ",inventoryContainer[slot].item.name, " Quantity in this slot: ", inventoryContainer[slot].count)
+	print("Line 27 | ", inventoryContainer[3])	
 	
 	add_item(tempItem)
-	print(inventoryContainer[3])
+	print("Line 30 | ",inventoryContainer[3])
 	
 	add_item(tempItem2)
 	for slot in inventoryContainer.size():
-		print("Slot ", slot, " ",inventoryContainer[slot]["item"].name, " Quantity in this slot: ", inventoryContainer[slot]["count"])
+		print("Line 34 | Slot ", slot, " ",inventoryContainer[slot].item.name, " Quantity in this slot: ", inventoryContainer[slot].count)
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -39,61 +39,68 @@ func _process(delta: float) -> void:
 
 func add_item(item: Resource) -> bool: # -> bool means you're going to return a boolean
 	if is_full() && is_stackable(item) != true: #If is_full returns true, which means the inventory is full, then return false to whatever called add_item and if is_stackable() is false, which means you can't stack any more in any of the slots with the item it then inventory is full
-		print("Inventory is full.")
+		print("Line 42 | Inventory is full.")
 		return false
 	
-	for slot in inventoryContainer:
-		if slot["item"] == null:
-			slot["item"] = item
-			slot["count"] = 1
-			return true
 	
 	#Check if the item exists and is stackable
 	for slot in inventoryContainer:
-
-		if slot["item"].id == item.id and slot["item"].stackable and slot["count"] <  item.max_stack: #This compares id's, check to see if the bool is true for the attribute stackable, and finally is stopped if max_stack is met
-			slot["count"] += 1 #Adds item to stack in current slot 
-			print("Line 39 | ID:", slot["item"].id, "| Name:", slot["item"].name, "| Count:", slot["count"])
+		if slot.item.id == item.id and slot.item.stackable and slot.count <  item.max_stack: #This compares id's, check to see if the bool is true for the attribute stackable, and finally is stopped if max_stack is met
+			slot.count += 1 #Adds item to stack in current slot 
+			print("Line 50 | ID:", slot.item.id, "| Name:", slot.item.name, "| Count:", slot.count)
 			return true
 			
 	
 	
-	#Otherwise, place in a new slot
-	inventoryContainer.append(
-		{"item": item, "count":1}
-	)
+	#if there's nothing in the array or it's under the inventoryMaxSize then append a new instance of InventorySlot
+	if inventoryContainer.size() < inventoryMaxSize:
+		inventoryContainer.append(
+			InventorySlot.new()
+		)
+	
+	#Find the InventorySlot that was just appended above here and change the default count to 1
+	for slot in inventoryContainer:
+		if slot.item == null:
+			slot.item = item
+			slot.count = 1
+			return true
+	
 	for slotNumber in inventoryContainer.size():
-		print("Slot ", slotNumber + 1, ": ", inventoryContainer[slotNumber]["item"].name, " Quantity in this slot: ", inventoryContainer[slotNumber]["count"])
+		print("Line 69 | Slot ", slotNumber + 1, ": ", inventoryContainer[slotNumber].item.name, " Quantity in this slot: ", inventoryContainer[slotNumber].count)
 	return true	
 	
 # Remove item that has been passed in when function was called from inventoryContainer array
-func remove_item(item: Resource, slotNumber, removeFromStackAmount):
+func remove_item(slotNumber, removeFromStackAmount):
 	if slotNumber >= inventoryContainer.size(): #If your slot number is out of range of the amount of locations in the inventoryContainer array then return false
-		print("Invalid slot number")
+		print("Line 75 | Invalid slot number")
 		return false
 	
-	var slot = inventoryContainer[slotNumber]
+	var slot = inventoryContainer[slotNumber] # Assign the array location that was chosen from the inventoryContainer to slot
 	
-	if slot["item"] == null:
-		print("Slot is already empty")
+	#if the slot that was chosen doesn't exist because it's null then return false
+	if slot.item == null:
+		print("Line 82 | Slot is already empty")
 		return false
 	
-	if slot["count"] <= removeFromStackAmount:
-		slot["item"] = null
-		slot["count"] = 0
-	else:
-		slot["count"] = slot["count"] - removeFromStackAmount
+	#if the slot count is less than or equal to the amount you want to remove then reset item and count to defaults
+	if slot.count <= removeFromStackAmount:
+		slot.item = null
+		slot.count = 0
+	else: #otherwise slot.count is more than the amount wanting to be removed so subtract the amount to be removed from the slot.count amount e.g. 3(slot.count) - 2(removeFromStackAmount) = 1(slot.count)
+		slot.count = slot.count - removeFromStackAmount
 	
 	return true
 	
 func is_full() -> bool:
 	return inventoryContainer.size() >= inventoryMaxSize #Apparently you can use an if statement on the return line and return if it's true or not.
-	# So if the inventory is full or over the MaxSize then return true to whatever called add_item
+	# So if the inventory is full or over the MaxSize then return true to whatever called add_item because this is a check at the beginning of the function
 
 func is_stackable(item: Resource) -> bool:
-	for slot in inventoryContainer: #iterate through the inventory array
-		if item.max_stack > slot["count"] && item == slot["item"]: #if the max_stack in the resource for the item is more than how much you have currently stacked on that item. Also for item == slot["item"] this ensures that the item being place into the invetory will be stacked as long as it's the same item
+	for slot in inventoryContainer: #iterate through the inventory array and assign a location to slot
+		if item.max_stack > slot.count && item == slot.item: #if the max_stack in the resource for the item is more than how much you have currently stacked on that item and item == slot.item return true, this ensures that the item being place into the invetory will be checked to ensure the stack doesn't go over item.max_stack and stacked as long as it's the same item
 			return true #return true, meaning you can place another item in the stack
-		elif slot["item"] == null:
+		elif slot.item == null: #if slot.item == null then you can place an item here
+			return true
+		elif slot.item.stackable: #if the variable, stackable, that's in the resource item is true return true
 			return true
 	return false #return false if the item you're placing in the inventory cannot be stacked because the max_stack has been met or was exceeded
