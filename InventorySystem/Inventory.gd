9b80d1658c14f740f320bb274c2612f6ab3c8926
B-Chavez <59714905+B-Chavez@users.noreply.github.com>
2025-06-04 20:48:56 -0700

    Refined add_item & is_stackable
    In add item the first check to see if the inventory is full also checks to see if the item can be stacked. I do this because when the inventory reaches the last slot and there's 1 item in the stack it thinks the inventory is full.
    
    is_stackable() now has something in it to complete the first check with is_full()

--- InventorySystem/Inventory.gd
+++ InventorySystem/Inventory.gd
@@ -16,24 +16,29 @@ func _ready() -> void:
 	for slot in inventoryContainer.size():
 		print(inventoryContainer[slot])
 	
-	for i in 10:
+	for i in 50:
 		add_item(tempItem)
 	pass # Replace with function body.
 
+	remove_item(tempItem)
+	print("Amount of filled slots in inventory: ", inventoryContainer.size())
+	for slot in inventoryContainer.size():
+		print(inventoryContainer[slot]["item"].name, " Quantity in this slot: ", inventoryContainer[slot]["count"])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func add_item(item: Resource) -> bool: # -> bool means you're going to return a boolean
-	if is_full(): #If is_full returns true, which means the inventory is full, then return false to whatever called add_item
+	if is_full() && is_stackable(item) != true: #If is_full returns true, which means the inventory is full, then return false to whatever called add_item
+		print("Inventory is full.")
 		return false
 	
 	#Check if the item exists and is stackable
 	for slot in inventoryContainer:
 
 		if slot["item"].id == item.id and slot["item"].stackable and slot["count"] <  item.max_stack: #This compares id's, check to see if the bool is true for the attribute stackable, and finally is stopped if max_stack is met
-			slot["count"] += 1
+			slot["count"] += 1 #Adds item to stack in current slot 
 			print("Line 39 | ID:", slot["item"].id, "| Name:", slot["item"].name, "| Count:", slot["count"])
 
 			return true
@@ -42,7 +47,8 @@ func add_item(item: Resource) -> bool: # -> bool means you're going to return a 
 	inventoryContainer.append(
 		{"item": item, "count":1}
 	)
-	print("Line 39 ", inventoryContainer)
+	for slotNumber in inventoryContainer.size():
+		print("Slot ", slotNumber + 1, ": ", inventoryContainer[slotNumber]["item"].name, " Quantity in this slot: ", inventoryContainer[slotNumber]["count"])
 	return true	
 	
 # Remove item that has been passed in when function was called from inventoryContainer array
@@ -56,5 +62,8 @@ func is_full() -> bool:
 	return inventoryContainer.size() >= inventoryMaxSize #Apparently you can use an if statement on the return line and return if it's true or not.
 	# So if the inventory is full or over the MaxSize then return true to whatever called add_item
 
-func is_stackable() -> bool:
-	return inventoryContainer[0]
+func is_stackable(item: Resource) -> bool:
+	for slot in inventoryContainer: #iterate through the inventory array
+		if item.max_stack > slot["count"] && item == slot["item"]: #if the max_stack in the resource for the item is more than how much you have currently stacked on that item. Also for item == slot["item"] this ensures that the item being place into the invetory will be stacked as long as it's the same item
+			return true #return true, meaning you can place another item in the stack
+	return false #return false if the item you're placing in the inventory cannot be stacked 
